6602e3df5d678e680a21f6c6e579afee7557be3a
syedsarin <syedsarinmehdi@gmail.com>
2024-07-13 00:09:59 +0530

    Changes in Sum Method by TL at line no 6

--- GitMavenPtroj01/src/main/java/com/ssm/service/Arethmetic.java
+++ GitMavenPtroj01/src/main/java/com/ssm/service/Arethmetic.java
@@ -3,6 +3,7 @@ package com.ssm.service;
 public class Arethmetic {
 	
 	public int doSum(int x,int y) {
+		System.out.println("Arethmetic.doSum() Begin");
 		System.out.println("Arethmetic.doSum() Start");
 		return x+y;
 	}
