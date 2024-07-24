72662e75b81f84c3c18ce6d4b7d5402940461dd3
syedsarin <syedsarinmehdi@gmail.com>
2024-07-24 12:34:28 +0530

    Springboot Data Rendering Controller Methods Behaviour

---
+++ Springboot-MVC-Proj02-Data_Rendering/src/main/java/com/ssm/SpringbootMvcProj02DataRenderingApplication.java
@@ -0,0 +1,13 @@
+package com.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcProj02DataRenderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcProj02DataRenderingApplication.class, args);
+	}
+
+}
